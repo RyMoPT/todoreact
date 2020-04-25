08a3c7dbbb00c5bfaad93cef093241cac6a653f2
rymopt <rmoham24@uwo.ca>
2020-04-25 15:30:13 -0400

    Updates

---
+++ precache-manifest.90c02a1c41dd30a2f8284f090a677c21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5104e626e1e294b00dc448aede58214d",
+    "url": "/todoreact/index.html"
+  },
+  {
+    "revision": "db301716d0dc14a4a4e8",
+    "url": "/todoreact/static/css/main.80d4f98b.chunk.css"
+  },
+  {
+    "revision": "8c00b32fa49762ad5362",
+    "url": "/todoreact/static/js/2.e5e09da7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoreact/static/js/2.e5e09da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db301716d0dc14a4a4e8",
+    "url": "/todoreact/static/js/main.80b55c6b.chunk.js"
+  },
+  {
+    "revision": "816386361e5686b4dcd6",
+    "url": "/todoreact/static/js/runtime-main.b313fa6c.js"
+  }
+]);
